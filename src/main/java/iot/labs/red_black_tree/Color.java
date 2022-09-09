727f123f9b1aa261bd404db5a754658bc1df2854
Danylo Kovalchuk <danivushka@gmail.com>
2022-09-09 04:16:49 +0300

    added lab 1 (red-black tree) code

---
+++ src/main/java/iot/labs/red_black_tree/Color.java
@@ -0,0 +1,5 @@
+package main.java.iot.labs.red_black_tree;
+
+public enum Color {
+    RED,BLACK
+}
---
+++ src/main/java/iot/labs/red_black_tree/Main.java
@@ -0,0 +1,31 @@
+package main.java.iot.labs.red_black_tree;
+
+public class Main {
+
+    public static void main(String[] args) {
+	RedBlackTree<Integer> lolBlackTreee = new RedBlackTree<>();
+	lolBlackTreee.insert(128);
+	lolBlackTreee.insert(64);
+	lolBlackTreee.insert(32);
+	lolBlackTreee.insert(15);
+	lolBlackTreee.insert(20);
+	lolBlackTreee.insert(48);
+	lolBlackTreee.insert(91);
+	lolBlackTreee.insert(5);
+	lolBlackTreee.insert(78);
+	lolBlackTreee.insert(45);
+	lolBlackTreee.insert(45);
+	lolBlackTreee.insert(69);
+	lolBlackTreee.insert(25);
+	lolBlackTreee.insert(789);
+	lolBlackTreee.insert(152);
+	lolBlackTreee.insert(335);
+	lolBlackTreee.insert(66);
+	lolBlackTreee.insert(99);
+	lolBlackTreee.insert(1);
+	lolBlackTreee.insert(85);
+	lolBlackTreee.insert(76);
+	lolBlackTreee.print();
+    }
+
+}
